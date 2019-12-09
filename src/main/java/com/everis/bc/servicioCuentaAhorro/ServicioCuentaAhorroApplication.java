9d63be42bd3cbb56d3ff44dd3ff5e2e04236b62c
fjosescarmona <fjosescarmona@gmail.com>
2019-12-09 17:52:23 -0500

    primera carga servicio cuenta ahorro

---
+++ src/main/java/com/everis/bc/servicioCuentaAhorro/ServicioCuentaAhorroApplication.java
@@ -0,0 +1,13 @@
+package com.everis.bc.servicioCuentaAhorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCuentaAhorroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioCuentaAhorroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/bc/servicioCuentaAhorro/ServicioCuentaAhorroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.bc.servicioCuentaAhorro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioCuentaAhorroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
